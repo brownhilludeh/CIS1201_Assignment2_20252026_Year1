ea0758f84636f5686229b677fa12c4ea261e1f05
brownhilludeh <budeh@hollandcollege.com>
2025-10-07 15:32:20 -0300

    Assignment2 Done Modified and updated

--- src/main/java/ca/hccis/Controller.java
+++ src/main/java/ca/hccis/Controller.java
@@ -1,12 +1,22 @@
 package ca.hccis;
 
+import java.util.Scanner;
+
 import ca.hccis.entity.PetCare;
 
 public class Controller {
     public static void main(String[] args) {
 
-        PetCare petCare = new PetCare();
-        petCare.getInformation();
-        petCare.display();
+        Scanner input = new Scanner(System.in);
+
+        String options;
+
+do {
+    PetCare petCare = new PetCare();
+
+    petCare.getInformation();
+    petCare.display();
+
+} while (!opted.equals("y"));
     }
 }
